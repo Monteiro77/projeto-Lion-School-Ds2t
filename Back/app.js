d3cda0039c86bd529b4729d036732335216e0198
VINÍCIUS MONTEIRO ARAÚJO <22282226@CFP127ALUNO.LOCAL>
2023-03-27 16:57:48 -0300

    back teminado

--- Back/app.js
+++ Back/app.js
@@ -6,6 +6,7 @@
  *****************************************************************************************************/
 //Import das dependencias do projeto
 const cursos = require('./module/module.js')
+
 //Import do express
 const express = require('express');
 //Import Cors
@@ -50,6 +51,98 @@ app.get('/v1/lion-school/cursos', cors(), async function (request,response, next
     
 })
 
+
+app.get('/v1/lion-school/alunos', cors(), async function (request, response, next){
+
+    let statusCode
+    let dadosAluno = {}
+
+    let aluno = cursos.getInfoAluno()
+
+    if(aluno){
+        statusCode = 200
+        dadosAluno = aluno
+    }
+    else{
+        statusCode = 500
+    }
+
+    response.status(statusCode)
+    response.json(dadosAluno)
+})
+
+app.get('/v1/lion-school/alunos/:matricula', cors(), async function (request, response, next){
+    let statusCode
+    let dadosAluno = {}
+
+    let matriculaAluno = request.params.matricula
+
+        if(matriculaAluno == "" || matriculaAluno == undefined || isNaN(matriculaAluno)){
+            statusCode = 400
+            dadosAluno.message = 'Não foi possível acessar pois os dados de entrada (matricula) não corresponde ao exigido, confira o valor, pois não pode ser vazio e precisar ser a caracteres'
+        }else{
+            let aluno = cursos.getMatricula(matriculaAluno)
+
+            if(aluno){
+                statusCode = 200
+                dadosAluno = aluno
+            }else{
+                statusCode = 400
+            }
+
+            response.status(statusCode)
+            response.json(dadosAluno)
+        }
+    
+})
+
+app.get('/v1/lion-school/aluno', cors(), async function (request, response, next){
+    let statusCode
+    let dadosCurso = {}
+
+    let siglaCurso = request.query.curso
+
+    if(siglaCurso == "" || siglaCurso == undefined || !isNaN(siglaCurso)){
+        statusCode = 400
+        dadosCurso.message = 'Não foi possível fazer a busca, pois os dados de entrada(curso) estão incorretos'
+    }else{
+        let infomacoesCurso = cursos.getAlunosCurso(siglaCurso)
+
+        if(infomacoesCurso){
+            statusCode = 200
+            dadosCurso = infomacoesCurso
+        }else{
+            statusCode = 400
+        }
+
+        response.status(statusCode)
+        response.json(dadosCurso)
+    }
+})
+
+app.get('/v1/lion-school/aluno/info', cors(), async function (request, response,next){
+    let statusCode 
+    let dadosAluno = {}
+
+    let statusAluno = request.query.status
+
+    if(statusAluno == "" || statusAluno == undefined || !isNaN(statusAluno)){
+        statusCode = 400
+        dadosAluno.message = 'Não foi possível fazer a busca, pois os dados de entrada(curso) estão incorretos' 
+    }else{
+        let infomacoesAluno = cursos.getStatusAluno(statusAluno)
+
+        if(infomacoesAluno){
+            statusCode = 200
+            dadosAluno = infomacoesAluno
+        }else{
+            statusCode = 400
+        }
+
+        response.status(statusCode)
+        response.json(dadosAluno)
+    }
+})
 app.listen(8080, function(){
     console.log('SERVIDOR AGUARDANDO REQUISIÇÕES NA PORTA 8080')
 })
--- Back/module/module.js
+++ Back/module/module.js
@@ -19,6 +19,8 @@ const getCursos = function() {
 
 const getInfoAluno = function() {
     let listaInformacoesJson = {}
+    let informacoes = []
+    let alunos = {}
 
     listaAlunos.alunos.forEach(function(lista){
         listaInformacoesJson = 
@@ -28,14 +30,119 @@ const getInfoAluno = function() {
             matricula: lista.matricula,
             sexo: lista.sexo
         }
-
-        
+        informacoes.push(listaInformacoesJson)
     })
-
+    alunos = {
+        informacoes
+    }
+    return alunos
 
 }
 
-getInfoAluno();
+const getMatricula = function(matricula){
+    matriculaAluno = matricula
+    let listaInformacoesJson = {}
+    let informacoesDisciplinasArray = []
+    let informacoesDisciplinasJson
+    
+    
+
+    listaAlunos.alunos.forEach(function(lista){
+        if(matriculaAluno == lista.matricula){
+            listaInformacoesJson = {
+                nome: lista.nome,
+                foto: lista.foto,
+                matricula: lista.matricula,
+                sexo: lista.sexo,
+                nomeCurso: lista.curso[0].nome,
+                sigla: lista.curso[0].sigla,
+                icone: lista.curso[0].icone,
+                conclusao: lista.curso[0].conclusao
+            }
+
+            lista.curso[0].disciplinas.forEach(function(listaDisciplina){
+                listaInformacoesJson.disciplinas = informacoesDisciplinasArray
+
+                informacoesDisciplinasJson = {
+                    nomeDisciplina: listaDisciplina.nome,
+                    media: listaDisciplina.media,
+                    status: listaDisciplina.status
+                }
+                informacoesDisciplinasArray.push(informacoesDisciplinasJson)
+            })
+
+            
+        }
+    })
+    
+
+    return listaInformacoesJson
+
+}
+
+const getAlunosCurso = function(siglaCurso){
+
+    let cursoSigla = siglaCurso.toUpperCase()
+    let listaInformacoesJson = {}
+    let informacoes = []
+    let alunos= {}
+
+    let cursoAluno = listaAlunos.alunos
+
+    cursoAluno.forEach(function(lista){
+        if(cursoSigla == lista.curso[0].sigla){
+            listaInformacoesJson = {
+                nome: lista.nome,
+                foto: lista.foto,
+                matricula: lista.nome,
+                sexo: lista.sexo
+            }
+            informacoes.push(listaInformacoesJson)
+        }
+    })
+    alunos = {
+        informacoes
+    }
+    return alunos
+}
+
+const getStatusAluno = function(status){
+
+    let statusAluno = status
+    let listaInformacoesJson = {}
+    let informacoes = []
+    let alunos = {}
+
+
+    let aluno = listaAlunos.alunos
+
+    aluno.forEach(function(lista){
+        if(statusAluno.toUpperCase() == lista.status.toUpperCase()){
+            listaInformacoesJson = {
+                nome: lista.nome,
+                foto: lista.foto,
+                matricula: lista.matricula,
+                sexo: lista.sexo,
+                status: lista.status
+            }
+            informacoes.push(listaInformacoesJson)
+        }
+    })
+
+    alunos = {
+        informacoes
+    }
+
+    return alunos
+
+}
+
+
+
 module.exports = {
-    getCursos
+    getCursos,
+    getInfoAluno,
+    getMatricula,
+    getAlunosCurso,
+    getStatusAluno
 }

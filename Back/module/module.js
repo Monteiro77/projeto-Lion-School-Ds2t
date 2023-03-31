9b6ab5067cdba1e7815aa88c20d0925d8186692a
Vinícius Monteiro <vmonteiro7843@gmail.com>
2023-03-31 15:23:55 -0300

    back finalizado pt12

--- Back/module/module.js
+++ Back/module/module.js
@@ -100,7 +100,7 @@ const getAlunosCurso = function(siglaCurso){
                 sexo: lista.sexo,
                 status: lista.status,
                 curso: lista.curso[0].nome,
-                dataConclusão: lista.curso[0].conclusao
+                dataConclusago: lista.curso[0].conclusao
 
             }
             informacoes.push(listaInformacoesJson);
